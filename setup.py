f3496c86c53b8b5f463a922f3815acf58c92f013
Alexander Couzens <lynxis@fe80.eu>
2020-05-31 04:42:55 +0200

    setup.py: fix project url
    It was still on the pyusb website, which was used
    as template

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Adlink USB DAQ driver',
     author='Alexander Couzens',
     author_email='lynxis@fe80.eu',
-    url='https://pyusb.github.io/pyusb',
+    url='https://github.com/lynxis/adlinkusb',
     packages=['adlinkusb'],
     long_description=
 """
