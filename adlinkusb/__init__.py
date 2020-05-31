4260a3c4acb4c9c13308b0d8bb6fb1ebe2008f5f
Alexander Couzens <lynxis@fe80.eu>
2020-05-31 04:47:51 +0200

    adlinkusb: set version 0.0.2

--- adlinkusb/__init__.py
+++ adlinkusb/__init__.py
@@ -2,7 +2,7 @@
 from .daq7250 import DAQ7250
 
 # Use Semantic Versioning, http://semver.org/
-version_info = (0, 0, 1)
+version_info = (0, 0, 2)
 __version__ = '%d.%d.%d' % version_info
 
 __all__ = [ "DAQ7250" ]
